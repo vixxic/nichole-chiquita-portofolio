635ecbf9e40d7c316d4ae017ee6bd0b5a73825f4
vixxic <janikanilobak@gmail.com>
2026-07-09 12:38:25 +0700

    new project

---
+++ src/projectData.jsx
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "foodi e-commerce",
+    image: "/projects/foodi.png",
+    desc: "e-commerce and landing page website",
+    link: "https://foodi-restaurant-ecommerce.netlify.app/",
+  },
+  {
+    title: "Sports News",
+    image: "/projects/sports-news.png",
+    desc: "News and article about sports",
+    link: "https://rpl10-final-exam.netlify.app/",
+  },
+  {
+    title: "Team.",
+    image: "/projects/team.-company.png",
+    desc: "Team. company's mock website",
+    link: "https://rpl10-final-exam.netlify.app/",
+  },
+  {
+    title: "Cimoly Yogurt Product",
+    image: "/projects/cimoly-product-page.png",
+    desc: "Cimoly sigle page website",
+    link: "https://gsap-nimi-project-2-nichole.netlify.app/",
+  },
+  {
+    title: "Candy Cloud Cup",
+    image: "/projects/cloudsip-product-page.png",
+    desc: "Cloudsip sigle page website",
+    link: "https://candy-cloud-cup.netlify.app/",
+  },
+  {
+    title: "Produx Dashboard",
+    image: "/projects/produx-dashboard.png",
+    desc: "Simple dashboard",
+    link: "https://midterm-project-rpl10-2-nichole.netlify.app/login",
+  },
+  {
+    title: "Portofolio V2",
+    image: "/projects/portofolio-v2.png",
+    desc: "Portofolio lainya",
+    link: " https://project-portofolio-nichole.vercel.app/",
+  },
+];
--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -6,6 +6,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+import { projects } from "../projectData";
+
 import "../styles/Project.css";
 import { IoMdOpen } from "react-icons/io";
 
@@ -51,39 +53,6 @@ function Project() {
       },
     });
   }, []);
-
-  const projects = [
-    {
-      title: "Team.",
-      image: "/projects/team.-company.png",
-      desc: "Team. company's mock website",
-      link: "https://rpl10-final-exam.netlify.app/",
-    },
-    {
-      title: "Cimoly Yogurt Product",
-      image: "/projects/cimoly-product-page.png",
-      desc: "Cimoly sigle page website",
-      link: "https://gsap-nimi-project-2-nichole.netlify.app/",
-    },
-    {
-      title: "Candy Cloud Cup",
-      image: "/projects/cloudsip-product-page.png",
-      desc: "Cloudsip sigle page website",
-      link: "https://candy-cloud-cup.netlify.app/",
-    },
-    {
-      title: "Produx Dashboard",
-      image: "/projects/produx-dashboard.png",
-      desc: "Simple dashboard",
-      link: "https://midterm-project-rpl10-2-nichole.netlify.app/login",
-    },
-    {
-      title: "Portofolio V2",
-      image: "/projects/portofolio-v2.png",
-      desc: "Portofolio lainya",
-      link: " https://project-portofolio-nichole.vercel.app/",
-    },
-  ];
 
   return (
     <div id="project" className="projects-section section poppins-regular">
